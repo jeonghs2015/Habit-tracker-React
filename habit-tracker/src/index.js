b003ea2779670b46e7544d99779ee9ae6bcbde8b
hsjung2015 <hsjung201558@gmail.com>
2022-08-31 16:13:42 +0900

    habit 관련 css 추가 작업

--- habit-tracker/src/index.js
+++ habit-tracker/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app.jsx';
+import '@fortawesome/fontawesome-free/js/all.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
