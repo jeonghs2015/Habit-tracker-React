0e47d7b9fa3ab85227b188efcad8b2d001868016
hsjung2015 <hsjung201558@gmail.com>
2022-08-31 17:35:17 +0900

    state 설정 , unique key 설정

--- habit-tracker/src/components/habit.jsx
+++ habit-tracker/src/components/habit.jsx
@@ -15,10 +15,13 @@ class Habit extends Component {
         this.setState({count: count < 0 ? 0 : count});
     }
   render() {
+    console.log(this.props.habit);
+    const habitName = this.props.habit.name;
+    const { name, count } = this.props.habit;
     return (
     <li className="habit">
-        <span className="habit-name">Reading</span>
-        <span className="habit-count">{this.state.count}</span>
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
         <button className="habit-button habit-increase" onClick={this.handleIncrement}>
             <i className="fas fa-plus-square"></i>
         </button>
